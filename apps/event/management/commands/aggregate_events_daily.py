a1f8446275bc141acd1329dc350ede7a5c87730a
Johan Lundberg <lundberg@nordu.net>
2013-09-12 17:11:14 +0200

    Removed unused imports and redundant code.

--- apps/event/management/commands/aggregate_events_daily.py
+++ apps/event/management/commands/aggregate_events_daily.py
@@ -2,11 +2,9 @@
 __author__ = 'lundberg'
 
 from django.core.management.base import BaseCommand, CommandError
-from django.db import IntegrityError
 from django.db.models import Count
 from dateutil.tz import tzutc
 from apps.event.models import Event, DailyEventAggregation
-from apps.event.views import get_protocol
 from datetime import datetime, timedelta
 
 
@@ -19,7 +17,7 @@ class Command(BaseCommand):
             if args[0] == 'yesterday':
                 today = datetime.now(tzutc()).replace(hour=0, minute=0, second=0, microsecond=0)
                 yesterday = today - timedelta(days=1)
-                qs = Event.objects.all().filter(ts__range=(yesterday, today)).extra(
+                qs = Event.objects.filter(ts__range=(yesterday, today)).extra(
                     {'date': 'date(ts)'}).values(
                         'date', 'origin__uri', 'rp__uri', 'protocol').annotate(num_events=Count('id'))
             elif args[0] == 'all':
@@ -37,8 +35,6 @@ class Command(BaseCommand):
                 if not created:
                     de.num_events = event_aggr['num_events']
                     de.save()
-        except ValueError:
-            raise CommandError('Could not make sense of the max or min days.')
         except IndexError:
             raise CommandError('Please run the command as: \
 "aggregate_events_daily yesterday" or "aggregate_events_daily all"')
