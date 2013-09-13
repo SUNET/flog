3c8b700a04922d554242cdc0832372ad68d29abc
Johan Lundberg <lundberg@nordu.net>
2013-09-13 16:08:54 +0200

    Duplicates are now identified by looking at success or failure too.

--- apps/event/management/commands/remove_duplicated_eduroam_events.py
+++ apps/event/management/commands/remove_duplicated_eduroam_events.py
@@ -6,6 +6,10 @@ from dateutil.tz import tzutc
 from apps.event.models import EduroamEvent
 from datetime import datetime, timedelta
 
+#
+# Duplicated is apparently a event with same csi with in 5 minutes of each other.
+#
+
 
 class Command(BaseCommand):
     args = 'yesterday|all'
@@ -16,11 +20,21 @@ class Command(BaseCommand):
             if args[0] == 'yesterday':
                 today = datetime.now(tzutc()).replace(hour=0, minute=0, second=0, microsecond=0)
                 yesterday = today - timedelta(days=1)
-                events = EduroamEvent.objects.filter(ts__range=(yesterday, today))
+                successful_events = EduroamEvent.objects.filter(ts__range=(yesterday, today), successful=True)
+                fail_events = EduroamEvent.objects.filter(ts__range=(yesterday, today), successful=False)
             elif args[0] == 'all':
-                events = EduroamEvent.objects.all()
+                successful_events = EduroamEvent.objects.filter(successful=True)
+                fail_events = EduroamEvent.objects.filter(successful=False)
 
-            for event in events:
+            for event in successful_events:
+                qs = EduroamEvent.objects.filter(ts__lt=event.ts + timedelta(minutes=5),
+                                                 ts__gt=event.ts - timedelta(minutes=5),
+                                                 calling_station_id=event.calling_station_id).order_by('ts')
+                if qs.count() > 1:
+                    for dup in qs[1:]:
+                        dup.delete()
+
+            for event in fail_events:
                 qs = EduroamEvent.objects.filter(ts__lt=event.ts + timedelta(minutes=5),
                                                  ts__gt=event.ts - timedelta(minutes=5),
                                                  calling_station_id=event.calling_station_id).order_by('ts')
