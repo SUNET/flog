11989641d64d83d873d583f5dc9d0dc0d88df82b
Johan Lundberg <lundberg@nordu.net>
2014-06-13 15:59:48 +0200

    Helpfull error message when check api fails to find uri or realm

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,18 +1,22 @@
 '''
 Created on Apr 13, 2012
 
-@author: leifj
+@author: leifj, lundberg
 '''
 
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.exceptions import ObjectDoesNotExist
 from apps.event.models import import_events
 from apps.event.models import Entity, EduroamRealm
 import json
 from datetime import datetime, timedelta
+
+
+def normalize_whitespace(s):
+    return ' '.join(s.split())
+
 
 @csrf_exempt
 def iprt(request):
@@ -26,7 +30,19 @@ def iprt(request):
 def eduroamcheck(request):
     now = datetime.utcnow()
     realm = request.GET.get('realm', None)
-    realm = get_object_or_404(EduroamRealm, realm=realm)
+    try:
+        realm = EduroamRealm.objects.get(realm=realm)
+    except ObjectDoesNotExist:
+        msg = '''
+            You can find documentation for setting up F-ticks for eduroam at
+            https://confluence.terena.org/display/H2eduroam/How+to+deploy+eduroam+at+national+level
+            '''
+        result = {
+            'status': 404,
+            'error': 'Could not find realm %s' % realm,
+            'message': normalize_whitespace(msg)
+        }
+        return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=404, mimetype='application/json')
     result = {
         'latest_realm_event': None,
         'latest_institution_event': None
@@ -45,7 +61,19 @@ def eduroamcheck(request):
 def webssocheck(request):
     now = datetime.utcnow()
     uri = request.GET.get('uri', None)
-    entity = get_object_or_404(Entity, uri=uri)
+    try:
+        entity = Entity.objects.get(uri=uri)
+    except ObjectDoesNotExist:
+        msg = '''
+            You can find documentation for setting up F-ticks for WebSSO at
+            https://portal.nordu.net/display/SWAMID/SAML+f-ticks+for+Shibboleth
+            '''
+        result = {
+            'status': 404,
+            'error': 'Could not find entity %s' % uri,
+            'message': normalize_whitespace(msg)
+        }
+        return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=404, mimetype='application/json')
     result = {
         'latest_origin_event': None,
         'latest_rp_event': None
