a561e0a01095fc4fc1ea3659ed598669bf9a1e1b
Johan Lundberg <lundberg@sunet.se>
2017-09-26 17:17:01 +0200

    mimetype is now content_type

--- flog/apps/api/views.py
+++ flog/apps/api/views.py
@@ -43,7 +43,7 @@ def eduroamcheck(request):
             'error': 'Could not find realm %s' % realm,
             'message': normalize_whitespace(msg)
         }
-        return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=404, mimetype='application/json')
+        return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=404, content_type='application/json')
     result = {
         'latest_realm_event': None,
         'latest_institution_event': None
@@ -56,7 +56,7 @@ def eduroamcheck(request):
         result['latest_institution_event'] = realm.institution_events.filter(ts__gt=now-timedelta(days=1)).latest('ts').ts
     except ObjectDoesNotExist:
         pass
-    return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=201, mimetype='application/json')
+    return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=201, content_type='application/json')
 
 
 def webssocheck(request):
@@ -74,7 +74,7 @@ def webssocheck(request):
             'error': 'Could not find entity %s' % uri,
             'message': normalize_whitespace(msg)
         }
-        return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=404, mimetype='application/json')
+        return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=404, content_type='application/json')
     result = {
         'latest_origin_event': None,
         'latest_rp_event': None
@@ -87,7 +87,7 @@ def webssocheck(request):
         result['latest_rp_event'] = entity.rp_events.filter(ts__gt=now-timedelta(days=1)).latest('ts').ts
     except ObjectDoesNotExist:
         pass
-    return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=201, mimetype='application/json')
+    return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=201, content_type='application/json')
 
 
 def importcheck(request, event_type):
@@ -104,10 +104,10 @@ def importcheck(request, event_type):
             'error': 'Could not find event type %s' % event_type,
             'message': normalize_whitespace(msg)
         }
-        return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=404, mimetype='application/json')
+        return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=404, content_type='application/json')
     result = {
         'event_type': event_type,
         'latest_event': latest_event.ts
     }
-    return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=201, mimetype='application/json')
+    return HttpResponse(json.dumps(result, cls=DjangoJSONEncoder), status=201, content_type='application/json')
 
