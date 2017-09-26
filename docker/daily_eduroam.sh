26cfb0617b4ac945aba46a82d74a9948ae9ada00
Johan Lundberg <lundberg@sunet.se>
2017-09-26 17:37:17 +0200

    No need to remove duplicated eduroam events as that is taken care of during import

--- docker/daily_eduroam.sh
+++ docker/daily_eduroam.sh
@@ -11,8 +11,8 @@ echo "Updating realm data"
 /opt/env/bin/python /opt/flog/manage.py update_realm_meta_data
 
 # Aggregate eduroam data
-echo "Removing duplicate eduroam F-TICKs"
-/opt/env/bin/python /opt/flog/manage.py remove_duplicated_eduroam_events 1
+#echo "Removing duplicate eduroam F-TICKs"
+#/opt/env/bin/python /opt/flog/manage.py remove_duplicated_eduroam_events 1
 echo "Aggregating eduroam data"
 /opt/env/bin/python /opt/flog/manage.py aggregate_eduroam_events_daily 1
 
