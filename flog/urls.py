9e775b3fc45c177439742cca67530352b2bed891
Johan Lundberg <lundberg@sunet.se>
2019-08-06 16:32:12 +0200

    Hotfix

--- flog/urls.py
+++ flog/urls.py
@@ -3,13 +3,12 @@ from __future__ import absolute_import
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
-from flog.apps.event import views as event_views
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', event_views.index),
+    url(r'^$', include('flog.apps.event.urls')),
     url(r'^event/', include('flog.apps.event.urls')),
     url(r'^api/', include('flog.apps.api.urls')),
 ]
